1b06aad51afe7f690991aff304ef459b3412af89
Hwj1220 <782835831@qq.com>
2018-10-13 15:35:37 +0800

    添加CSS世界学习文件夹

--- vue-sell-wegin/src/components/goods/goods.vue
+++ vue-sell-wegin/src/components/goods/goods.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="goods">goods</div>
+<div class="goods">
+  <div class="menu-wrapper">goods</div>
+  <div class="foods-wrapper">foods</div>
+</div>
 </template>
 <script>
 export default {
@@ -7,5 +10,17 @@ export default {
 }
 </script>
 <style lang="stylus" scoped>
-
+  .goods
+    display flex
+    position absolute
+    top 174px
+    bottom 46px
+    width 100%
+    overflow hidden
+    .menu-wrapper
+      flex: 0 0 80px
+      width 80px
+      background: #f3f5f7
+    .food-wrapper
+      flex: 0 0 80px;
 </style>
