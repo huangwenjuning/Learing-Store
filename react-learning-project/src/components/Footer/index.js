561fa1e32ce068db85dde6f647e89361926111e2
Hwj1220 <782835831@qq.com>
2019-07-21 13:37:03 +0800

    底部实现

--- react-learning-project/src/components/Footer/index.js
+++ react-learning-project/src/components/Footer/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-
+import './index.less';
 class Footer extends Component {
     render() {
         return (
-            <div>this is Footer</div>
+            <div className="footer">
+                版权所有：无骛学习使用
+            </div>
         )
     }
 }
--- react-learning-project/src/components/Header/index.js
+++ react-learning-project/src/components/Header/index.js
@@ -41,7 +41,7 @@ class Header extends Component {
                 <Row className="header-top">
                     <Col span={24}>
                         <span>欢迎，{ this.state.userName }</span>
-                        <a href="#">退出</a>
+                        <a href=" #">退出</a>
                     </Col>
                 </Row>
                 <Row className="breadcrumb">
